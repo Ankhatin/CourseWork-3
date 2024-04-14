785228e6ca872d8b957e7113fcb399a06ac4ab0a
Andrey Khatin <ankhatin@gmail.com>
2024-04-14 14:37:53 +0700

    change in main script

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ def main():
     transactions = functions.load_transactions('data\\operations.json')
     sorted_transactions = functions.sort_of_transactions(transactions)
     last_operations = functions.get_five_last_transactions(sorted_transactions)
+    print('Список из 5 успешных последних операций по счету\n')
     for oper in last_operations:
-        row_first, row_second, row_third = functions.make_up_date_to_print(oper)
+        row_first, row_second, row_third = functions.make_up_data_to_print(oper)
         print(f'{row_first}\n{row_second}\n{row_third}\n')
 
 

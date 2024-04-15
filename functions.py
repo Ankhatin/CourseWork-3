f6d962371457bf1409ea5868c8399de341b5d448
Andrey Khatin <ankhatin@gmail.com>
2024-04-15 13:38:30 +0700

    changes in main and functions

--- functions.py
+++ functions.py
@@ -1,15 +1,23 @@
 import json
 import datetime
+import os.path
 
 def load_transactions(file_name):
     '''
-    Функция открывает файл с данными вопросов и возвращает в виде списка словарей
+    Функция открывает файл с данными по операциям
+    и возвращает в виде списка словарей
     '''
-    with open(file_name, 'r', encoding="utf-8") as file:
+    path = os.path.dirname(os.path.abspath(file_name))
+    path = os.path.join(path, r'data\operations.json')
+    with open(path, 'r', encoding="utf-8") as file:
         transactions = json.load(file)
         return transactions
 
 def sort_of_transactions(transactions):
+    '''
+    Функция отфильтровывает пустые элементы словаря,
+    операции со статусом 'CANCELED' и сортирует элементы по дате
+    '''
     trans = list()
     for item in transactions:
         if item and item['state'] == "EXECUTED":
@@ -18,6 +26,9 @@ def sort_of_transactions(transactions):
     return transactions
 
 def get_five_last_transactions(transactions):
+    '''
+    Функция получает из списка операций последние 5 в обратном порядке
+    '''
     operations = transactions[-1:-6:-1]
     return operations
 
@@ -28,18 +39,24 @@ def make_up_data_to_print(oper):
     row_first, row_second, row_third  = '', '', ''
     #Получим дату из строки формата ISO и отсечем время
     if check_key_is_exist(oper, 'date'):
+        #Конвертируем дату из формата ISO строки в объект datetime и отсекаем время
         date = datetime.datetime.fromisoformat(oper['date']).date()
         date = date.strftime("%d.%m.%Y")
     else:
-        date = 'No date'
-    #Проверим наличие данных в операции и сформируем первую строку для вывода
+        date = 'Нет данных'
+    #Проверяем наличие данных в операции и формируем первую строку для вывода
     if check_key_is_exist(oper, 'description'):
         row_first = f'{date} {oper['description']}'
     if check_key_is_exist(oper, 'from'): #Проверим наличие поля "откуда" в операции и вставим в строку
         row_second = make_up_string(oper, 'from')
+    else:
+        row_second = 'Нет данных'
     row_second = row_second + ' -> '
-    if check_key_is_exist(oper, 'to'): #Проверим наличие поля "куда" в операции м вставим в строку
+    if check_key_is_exist(oper, 'to'): #Проверяем наличие поля "куда" в операции м вставляем в строку
         to_string = make_up_string(oper, 'to')
+        row_second = row_second + to_string
+    else:
+        to_string = 'Нет данных'
         row_second = row_second + to_string
     if check_key_is_exist(oper, 'operationAmount'): #Проверим наличие поля "сумма" и сформируем строку для вывода
         oper_amount = oper['operationAmount']
@@ -55,7 +72,7 @@ def make_up_string(oper, direction):
     проверяя наличие подстроки 'Счет' в данных из операции
     '''
     from_to_item = oper[direction]
-    if from_to_item.find('Счет') >= 0:
+    if from_to_item.find('Счет') >= 0: #Проверяем, являются ли данные счетом или картой
         count_string = make_up_count_string(from_to_item)
         return count_string
     else:
--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import functions
 
 
 def main():
-    transactions = functions.load_transactions('data\\operations.json')
-    sorted_transactions = functions.sort_of_transactions(transactions)
-    last_operations = functions.get_five_last_transactions(sorted_transactions)
-    print('Список из 5 успешных последних операций по счету\n')
-    for oper in last_operations:
+    transactions = functions.load_transactions('operations.json') #загружаем из файла список словарей
+    sorted_transactions = functions.sort_of_transactions(transactions) #сортируем транзакции
+    last_operations = functions.get_five_last_transactions(sorted_transactions) #получаем 5 последних
+    print('Список из 5 последних успешных операций по счету\n')
+    for oper in last_operations: #Пройдемся циклом по операциям и сформируем стркои для вывода
         row_first, row_second, row_third = functions.make_up_data_to_print(oper)
         print(f'{row_first}\n{row_second}\n{row_third}\n')
 
